f40cdcadf20e9a2ee10745202ba757073fad0bc4
saphan muganza petr <muganzas80@gmail.com>
2025-09-07 16:54:52 +0300

    CORS

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 // CORS configuration
 const corsOptions = {
-  origin: process.env.FRONTEND_URL || "http://localhost:3001",
+  origin: true,
   credentials: true,
 };
 app.use(cors(corsOptions));
